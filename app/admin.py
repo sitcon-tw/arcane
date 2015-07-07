023bc6abc860a788f9bb1ba53e471f0e2b6685f4
Philip Chou <pcchou@pcchou.me>
2015-07-07 18:11:23 +0800

    fixed admin and commented in urls.py for debug purpose

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(player)
+admin.site.register(card)
+admin.site.register(team)
--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-import app
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^player/', include(app.player.urls)),
-    url(r'^card/', include(app.card.urls)),
+    #url(r'^player/', include("app.player.urls")),
+    #url(r'^card/', include("app.card.urls")),
 ]
