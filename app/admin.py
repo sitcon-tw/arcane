3ce41d0db78e00566338692f59b3b4a714f72f98
Philip Chou <pcchou@pcchou.me>
2015-07-13 22:17:22 +0800

    updated admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(player)
-admin.site.register(card)
-admin.site.register(team)
+admin.site.register(Player)
+admin.site.register(Card)
+admin.site.register(Team)
