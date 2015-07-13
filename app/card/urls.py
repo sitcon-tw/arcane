107a07b8872f9554772856633fb48842c1ccc83e
Philip Chou <pcchou@pcchou.me>
2015-07-13 18:49:24 +0800

    added card and card.edit (without permission checks)

--- app/card/urls.py
+++ app/card/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import include, url
 
 
 urlpatterns = [
-    url(r'^generate', "app.card.views.gen", name="generate card"),
-    url(r'^get/', "app.card.views.get", name="get"),
-    url(r'^edit/', "app.card.views.edit", name="edit card")
+    url(r'^\w{16}$', "app.card.views.card", name="view card"),
+    url(r'^generate/\w{16}$', "app.card.views.gen", name="generate card"),
+    url(r'^get/\w{16}$', "app.card.views.get", name="get"),
+    url(r'^edit/\w{16}$', "app.card.views.edit", name="edit card")
 ]
--- app/card/views.py
+++ app/card/views.py
@@ -1,5 +1,43 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponseNotFound, HttpResponse
+from app.models import card as Card
+from app.card.forms import CardForm
+from django.core.exceptions import ObjectDoesNotExist
+import re
+
 def card(request):
-    pass
+    id = re.sub(r'/',"",re.sub(r'/card/',"", request.path)) # 我就是要用骯髒解你管我
+    try:
+        card = Card.objects.get(cid=id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound("<h1>Wrong card</h1><meta http-equiv=\"refresh\" content=\"3; url=/\">")
+    get_url = "http://" + request.META['HTTP_HOST'] + "/card/get/" + card.cid
+    return render(request, "card/card.html", locals())
 
 def edit(request):
+    id = re.sub(r'/',"",re.sub(r'/card/edit/',"", request.path)) # 我就是要用骯髒解你管我
+    try:
+        card = Card.objects.get(cid=id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound("<h1>Wrong card</h1><meta http-equiv=\"refresh\" content=\"3; url=/\">")
+
+    if not request.POST:
+        form = CardForm({"name": card.name, "value": card.value, "long_desc": card.long_desc, "active": card.active, "retrieved": card.retrieved, "modified_reason": ""})
+        return render(request, "card/edit.html", locals())
+    else:
+        form = CardForm(request.POST)
+        if form.is_valid():
+            card.name = form.cleaned_data["name"]
+            card.value = form.cleaned_data["value"]
+            card.long_desc = form.cleaned_data["long_desc"]
+            card.active = form.cleaned_data["active"]
+            card.modified_reason = form.cleaned_data["modified_reason"]
+            card.save()
+        return HttpResponse("<h1>Submitted.</h1><meta http-equiv=\"refresh\" content=\"3; url=/card/" + card.cid + "\">")
+
+def get(request):
     pass
+
+def gen(request):
+    pass
+
