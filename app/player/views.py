4b2f1dd17be6c8ad776b2021896e5017541328da
Philip Chou <pcchou@pcchou.me>
2015-07-15 16:25:56 +0800

    added checks to player view

--- app/player/views.py
+++ app/player/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from app.models import is_player
+from django.contrib.auth.models import User
+from app.models import Player, is_player
 
-@login_required(login_url="/user/login")
-def player(request):
-    if not is_player(request.user):
-        raise PermissionDenied
+def player(request, id=None):
+    if not request.user.is_authenticated():
+        try:
+            user = User.objects.get(username=id)
+        except ObjectDoesNotExist:
+            return render(request, "submit.html", {"content":"<h1>Wrong user</h1><meta http-equiv=\"refresh\" content=\"3; url=/\">", "title":"錯誤！"}, status=404)
+
+        return redirect("/user/login/" + id)
     else:
-        user = request.user
-        player = request.user.player
-        return render(request, 'player/player.html', locals())
+        if request.user.is_staff:
+            try:
+                user = User.objects.get(username=id)
+            except ObjectDoesNotExist:
+                return render(request, "submit.html", {"content":"<h1>Wrong user</h1><meta http-equiv=\"refresh\" content=\"3; url=/\">", "title":"錯誤！"}, status=404)
+
+            player = user.player
+            return render(request, 'player/player.html', locals())
+        elif is_player(request.user):
+            user = request.user
+            return render(request, 'player/player.html', locals())
 
 def edit(request):
     pass
