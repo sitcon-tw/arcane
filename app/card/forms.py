8d58929451915f2993d8b17c118a4a3f3a701904
Philip Chou <pcchou@pcchou.me>
2015-07-17 18:27:19 +0800

    removed unreasonable card form option

--- app/card/forms.py
+++ app/card/forms.py
@@ -6,4 +6,3 @@ class CardForm(forms.Form):
     value = forms.IntegerField(label="值", help_text="點數卡的數值", initial="5")
     long_desc = forms.CharField(max_length=200, widget=forms.Textarea(), label="說明", required=False)
     active = forms.BooleanField(label="開通", help_text="該點數卡是否可用", required=False)
-    retrieved = forms.BooleanField(label="提領", help_text="該點數卡是否被提領了", required=False)
