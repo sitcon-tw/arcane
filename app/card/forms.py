0cd37963552d686444aa07053cc7af8f48c7e410
Philip Chou <pcchou@pcchou.me>
2015-07-20 07:37:00 +0800

    fixed random bugs (card/views)

--- app/card/forms.py
+++ app/card/forms.py
@@ -3,7 +3,7 @@ from app.models import Player
 
 class CardForm(forms.Form):
     name = forms.CharField(max_length=32, label="名字", help_text="點數卡的名子", required=False)
-    value = forms.IntegerField(label="值", help_text="點數卡的數值", initial="5", max_value=2000, min_value=-2000)
+    value = forms.IntegerField(label="值", help_text="點數卡的數值", initial="64", max_value=2000, min_value=-2000)
     long_desc = forms.CharField(max_length=200, widget=forms.Textarea(), label="說明", required=False)
     active = forms.BooleanField(label="開通", help_text="該點數卡是否可用", required=False, initial=True)
 
