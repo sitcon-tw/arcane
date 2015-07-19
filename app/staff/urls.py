ee50d3167730c5b4c1f0d13afca8bcb0a5fdd8e8
azdkj532 <kanade123321@gmail.com>
2015-07-20 03:36:37 +0800

    add lite staff

--- app/staff/urls.py
+++ app/staff/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^$', "app.staff.views.dashboard", name="dashboard"),
     url(r'^gift$', "app.staff.views.gift", name="fast send"),
+    url(r'^lite/(?P<tt>\d)?$', 'app.staff.views.lite', name="lite"),
 ]
