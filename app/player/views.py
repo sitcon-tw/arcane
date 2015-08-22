d4738c59d5c6141af71e6f77a6d37355e31b0863
野生的松鼠 <kanade123321@gmail.com>
2015-08-22 15:15:07 +0800

    player page

--- app/player/views.py
+++ app/player/views.py
@@ -21,7 +21,6 @@ def player(request, id=None):
             sorted_players = list(user.player.team.player.all())
             sorted_players.sort(key=lambda x: x.points_acquired, reverse=True)
             this_records = History.objects.filter(user=user)
-            fun = True
             records = []
             for player_i in user.player.team.player.all():
                 records = records + list(History.objects.filter(user=player_i.user))
